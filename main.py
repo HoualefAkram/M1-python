2d3b0124e877b341160ae3dede913bfa0e7a598b
Houalef Akram <116724029+HoualefAkram@users.noreply.github.com>
2024-10-13 21:58:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import sin, cos, sqrt
+from math import log, sin, cos, sqrt
 
 
 #### EXO 1 ####
@@ -210,3 +210,58 @@ for i in range(k):
     output = output + val
     sign = sign * -1
 print(output)
+
+
+### EXO 10
+g = 9.81
+while True:
+    h0, ep, n = (
+        float(input("initial height: ")),
+        float(input("epsilon: ")),
+        int(input("n: ")),
+    )
+    if h0 > 0 and ep >= 0 and ep < 1 and n >= 0:
+        break
+v0 = sqrt(2 * g * h0)
+v = v0 * (ep**n)
+h = (v**2) / (2 * g)
+print(f"height: {h}")
+
+
+### EXO 11
+# Using a while loop
+g = 9.81
+while True:
+    h0, ep, h_fin = (
+        float(input("initial height: ")),
+        float(input("epsilon: ")),
+        float(input("final height: ")),
+    )
+    if ep >= 0 and ep < 1 and h0 > 0 and h_fin > 0 and h_fin < h0:
+        break
+v0 = sqrt(2 * g * h0)
+v = v0
+counter = 0
+while True:
+    counter = counter + 1
+    v = v * ep
+    h = (v**2) / (2 * g)
+    if h <= h_fin:
+        break
+print(f"n: {h_fin}")
+
+# Using direct calculations
+g = 9.81
+while True:
+    h0, ep, h_fin = (
+        float(input("initial height: ")),
+        float(input("epsilon: ")),
+        float(input("final height: ")),
+    )
+    if ep >= 0 and ep < 1 and h0 > 0 and h_fin > 0 and h_fin < h0:
+        break
+
+v1 = sqrt(2 * g * h_fin)
+v0 = sqrt(2 * g * h0)
+n = log(v1 / v0) / log(ep)
+print(f"n: {round(n)}")
